7d12b50e134813cd86c018052ccd5761300253ed
Luuk <luuk.geelen@hotmail.com>
2020-11-02 08:29:02 +0100

    Updated docstrings

--- model/tile.py
+++ model/tile.py
@@ -6,11 +6,11 @@ class Tile:
     def __init__(self, x, y, size, state='empty'):
         """Initialization of the tile.
         
-            Args:
-            - x: the horizontal coordinate (in px) of the tile (top left)
-            - y: the vertical coordinate (in px) of the tile (top left)
-            - size: size of the tile (in px)
-            - state: the state of the tile. This is either 'empty' (default), 'X' or 'O'
+        Args:
+        - x: the horizontal coordinate (in px) of the tile (top left)
+        - y: the vertical coordinate (in px) of the tile (top left)
+        - size: size of the tile (in px)
+        - state: the state of the tile. This is either 'empty' (default), 'X' or 'O'
         """
         self.x = x
         self.y = y
@@ -25,8 +25,8 @@ class Tile:
         """A player makes a move and now an empty tile becomes a filled tile,
         based on the symbol of the player.
         
-            Args:
-            - symbol: the symbol of the player (human or AI) that makes the move,
-                can be either 'X' or 'O'.
+        Args:
+        - symbol: the symbol of the player (human or AI) that makes the move,
+                  can be either 'X' or 'O'.
         """
         self.state = symbol
